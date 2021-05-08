58eeda00f8ba9c77ab15bb8fca4a2b224ceb1ba4
Dan <Stachd@gmail.com>
2021-05-08 12:58:02 -0500

    Update nightvisiongoggles-scope.py

--- nightvisiongoggles-scope.py
+++ nightvisiongoggles-scope.py
@@ -4,7 +4,7 @@ import picamera
 
 camera = picamera.PiCamera()
 camera.led = False
-camera.coloreffects = (128,0) # (128,0) creates a blk and green night vision effect
+camera.color_effects = (128,0) # (128,0) creates a blk and green night vision effect
 
 
 camera.start_preview()
